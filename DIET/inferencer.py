3e625a05489b80b4c379dc2c949325ad1c816f3b
cheesama <cheehoon12@hanmail.net>
2020-04-08 23:32:41 +0900

    implement start of inferencer

---
+++ DIET/inferencer.py
@@ -0,0 +1,48 @@
+from .DIET_lightning_model import DualIntentEntityTransformer
+
+import torch
+
+model = None
+
+def load_model(checkpoint_path:str):
+    model = DualIntentEntityTransformer.load_from_checkpoint(checkpoint_path)
+
+    return model
+
+def inference(text:str):
+    if model is None:
+        raise ValueError('model is not loaded, first call load_model(checkpoint_path)')
+
+    tokens = model.dataset.tokenize(text)
+    intent_result, entity_result = model.forward(tokens)
+
+    #model.dataset.intent_dict
+    #model.dataset.entity_dict
+
+    #rasa NLU result format
+    """
+    {
+        "entities": [
+            {
+                "start": 0,
+                "end": 0,
+                "value": "string",
+                "entity": "string",
+                "confidence": 0
+            }
+        ],
+        "intent": {
+            "confidence": 0.6323,
+            "name": "greet"
+        },
+        "intent_ranking": [
+            {
+                "confidence": 0.6323,
+                "name": "greet"
+            }
+        ],
+        "text": "Hello!"
+    }
+    """
+
+

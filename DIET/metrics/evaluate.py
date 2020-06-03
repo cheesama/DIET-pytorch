3f1dcd79de4b899f466bd6909021cbc0f456dc36
seujung <digit82@gmail.com>
2020-06-03 14:12:38 +0900

    add entity metric

--- DIET/metrics/evaluate.py
+++ DIET/metrics/evaluate.py
@@ -2,7 +2,7 @@ from tqdm import tqdm, trange
 from torch.utils.data import DataLoader
 
 from .metrics import show_rasa_metrics, confusion_matrix, pred_report
-
+from DIET.decoder import NERDecoder  
 import torch
 import logging
 import numpy as np
@@ -43,78 +43,57 @@ def show_intent_report(dataset, pl_module, tokenizer, file_name=None, output_dir
     target_names = list(label_dict.values())
     
     report = show_rasa_metrics(pred=preds, label=targets, labels=labels, target_names=target_names, file_name=file_name, output_dir=output_dir)
-    ## generate confusion matrix
-    # inequal_index = np.where(preds != targets)[0]
-    # inequal_dict = dict()
-    # for i in range(inequal_index.shape[0]):
-    #     idx = inequal_index[i].item()
-    #     pred = preds[idx]
-    #     if label_dict[pred] not in inequal_dict.keys():
-    #         inequal_dict[label_dict[pred]] = []
-    #     tmp_dict = dict()
-    #     tmp_dict['target'] = label_dict[targets[idx]]
-    #     tmp_dict['prob'] = round(logits[idx], 3)
-    #     tmp_dict['text'] = text[idx]
-    #     inequal_dict[label_dict[pred]].append(tmp_dict)
+
+def show_entity_report(dataset, pl_module, file_name=None, output_dir=None, cuda=True):
     
-    # cm_file_name = file_name.replace('.', '_cm.')
-    # cm_matrix = confusion_matrix(
-    #         pred=preds, label=targets, label_index=label_dict, file_name=cm_file_name, output_dir=None)
-    
-    # pred_report(inequal_dict, cm_matrix, file_name=cm_file_name.replace(
-    #         '.json', '.md'),  output_dir=output_dir)
+    ##generate rasa performance matrics
+    tokenizer = dataset.tokenizer
+    text = []
+    label_dict = dict()
+    pl_module.model.eval()
+    for k, v in pl_module.entity_dict.items():
+        label_dict[int(k)] = v
 
-# def show_entity_report(dataset, pl_module, file_name=None, output_dir=None, cuda=True):
-    
-#     ##generate rasa performance matrics
-#     tokenizer = dataset.tokenizer
-#     text = []
-#     label_dict = dict()
-#     pl_module.model.eval()
-#     for k, v in pl_module.entity_dict.items():
-#         label_dict[int(k)] = v
+    decoder = NERDecoder(label_dict, None)
+    dataloader = DataLoader(dataset, batch_size=32)
 
-#     decoder = NERDecoder(label_dict, None)
-#     dataloader = DataLoader(dataset, batch_size=32)
+    preds = list()
+    targets = list()
+    labels = set()
 
-#     preds = list()
-#     targets = list()
-#     labels = set()
+    for batch in tqdm(dataloader, desc="load entity dataset"):
+        inputs, intent_idx, entity_idx, token = batch
+        (input_ids, token_type_ids) = inputs
+        text.extend(token)
+        if cuda > 0:
+            input_ids = input_ids.cuda()
+            token_type_ids = token_type_ids.cuda()
+        _, entity_result = pl_module.model.forward(input_ids, token_type_ids)
 
-#     for batch in tqdm(dataloader, desc="load entity dataset"):
-#         inputs, intent_idx, entity_idx = batch
-#         (input_ids, token_type_ids) = inputs
-#         token = get_token_to_text(tokenizer, input_ids)
-#         text.extend(token)
-#         if cuda > 0:
-#             input_ids = input_ids.cuda()
-#             token_type_ids = token_type_ids.cuda()
-#         _, entity_result = pl_module.model.forward(input_ids, token_type_ids)
-
-#         entity_result = entity_result.detach().cpu()
-#         _, entity_indices = torch.max(entity_result, dim=-1)
+        entity_result = entity_result.detach().cpu()
+        _, entity_indices = torch.max(entity_result, dim=-1)
 
 
 
-#         for i in range(entity_idx.shape[0]):
-#             decode_original = decoder.process(input_ids[i].cpu().numpy(), entity_idx[i].numpy())
-#             decode_pred = decoder.process(input_ids[i].cpu().numpy(), entity_indices[i].numpy())
+        for i in range(entity_idx.shape[0]):
+            decode_original = decoder.process(input_ids[i].cpu().numpy(), entity_idx[i].numpy(), text[i])
+            decode_pred = decoder.process(input_ids[i].cpu().numpy(), entity_indices[i].numpy(), text[i])
 
-#             for origin in decode_original:
-#                 labels.add(origin['entity'])
-#                 find_idx = 0
-#                 for pred in decode_pred:
-#                     if origin['start'] == pred['start'] and origin['end'] == pred['end']:
-#                         preds.append(origin['entity'])
-#                         targets.append(origin['entity'])
-#                         find_idx += 1
-#                 if find_idx == 0:
-#                      preds.append('No_Entity')
-#                      targets.append(origin['entity'])
+            for origin in decode_original:
+                labels.add(origin['entity'])
+                find_idx = 0
+                for pred in decode_pred:
+                    if origin['start'] == pred['start'] and origin['end'] == pred['end']:
+                        preds.append(origin['entity'])
+                        targets.append(origin['entity'])
+                        find_idx += 1
+                if find_idx == 0:
+                     preds.append('No_Entity')
+                     targets.append(origin['entity'])
 
 
 
-#     report = show_rasa_metrics(pred=preds, label=targets, file_name=file_name, output_dir=output_dir)
+    report = show_rasa_metrics(pred=preds, label=targets, file_name=file_name, output_dir=output_dir)
 
 
 # def get_token_to_text(tokenizer, data):

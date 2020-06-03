c593c2e09ea24419177b3c7bd86f74a92b2436fb
seujung <digit82@gmail.com>
2020-06-03 14:27:34 +0900

    add entity

--- DIET/eval.py
+++ DIET/eval.py
@@ -36,4 +36,6 @@ class PerfCallback(Callback):
             self.output_dir  = folder_path[-1]
         self.output_dir = os.path.join(self.output_dir, 'results')
         intent_report_nm = self.report_nm.replace('.', '_intent.')
+        entity_report_nm = self.report_nm.replace('.', '_entity.')
         show_intent_report(dataset, pl_module, tokenizer, file_name=intent_report_nm, output_dir=self.output_dir, cuda=self.cuda)
+        show_entity_report(dataset, pl_module, file_name=entity_report_nm, output_dir=self.output_dir, cuda=self.cuda)
